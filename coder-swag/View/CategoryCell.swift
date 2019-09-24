3c4c024c3bb10a30d4f11e577c7065ae99ba724d
Mahesh Prasad <mahesh223prasad@gmail.com>
2019-09-25 00:04:17 +0530

    Project creation & IBOutlet

---
+++ coder-swag/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Mahesh Prasad on 24/09/19.
+//  Copyright © 2019 Mahesh Prasad. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
