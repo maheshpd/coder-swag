6f9afeef850303e86c455f225f3c9391c75d1eb3
Mahesh Prasad <mahesh223prasad@gmail.com>
2019-09-27 00:13:53 +0530

    Creating a UICollectionView

---
+++ coder-swag/View/ProductDell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductDell.swift
+//  coder-swag
+//
+//  Created by Mahesh Prasad on 27/09/19.
+//  Copyright © 2019 Mahesh Prasad. All rights reserved.
+//
+
+import UIKit
+
+class ProductDell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
